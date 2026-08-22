7f7956089c43fe3dadc565328f7d734cfa11c090
vyctorbrzezowski <krzyszchweski@gmail.com>
2026-08-22 09:39:43 -0300

    Refine audio attachment controls

--- ui/src/i18n/locales/en.ts
+++ ui/src/i18n/locales/en.ts
@@ -5752,6 +5752,8 @@ export const en: TranslationMap = {
       play: "Play",
       pause: "Pause",
       seek: "Seek media",
+      mute: "Mute",
+      unmute: "Unmute",
       volume: "Volume",
       download: "Download {filename}",
       preparing: "Preparing playback…",
